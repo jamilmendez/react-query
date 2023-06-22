b9c4ae0fddb7bc83e2cc2db241b2e1aaef510f9c
jamilmendez <mdztnr99@gmail.com>
2023-06-22 10:11:43 -0500

    git token env

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: 'Bearer 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',
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   }
 });
